037beefd5c6d501baa86cb0cea63c64e07475101
Aryan <chaudharyaryan3456@gmail.com>
2025-07-20 14:47:09 +0530

    Practicing Project again

--- app.js
+++ app.js
@@ -2,22 +2,38 @@
 const express = require("express");//Step1
 const app = express();//Step1
 const mongoose = require("mongoose");//Step4(i)
+const Listing = require("./Models/listing.js");//Step5
+
+
+app.get("/", (req, res)=> {
+    res.send("Root is working");//Step3
+});
 
 const MONGO_URL =  "mongodb://127.0.0.1:27017/Practice3";//Step4(ii)
-
 main()
 .then((res) => {
     console.log("Connected to db");//Step4(iv)
 })
 .catch(err => console.log(err));
-
 async function main(){
     await mongoose.connect(MONGO_URL);//Step4(iii)
 }
 
-app.get("/", (req, res)=> {
-    res.send("Root is working");//Step3
-});
+//Test Route (step6)
+// app.get("/testlistings",async (req, res)=> {
+//     let sampleListing = new Listing({
+//         title: "Foodie World",
+//         description: "Tastes better than the thoughts",//Step6
+//         price: 570,
+//         location: "Manhattan, California",
+//         country: "United States of America"
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample was saved");
+//     res.send("Sample was tested");
+// });
+
 
 app.listen(8080, () => {
     console.log("Server is listening to port 8080");//Step2
